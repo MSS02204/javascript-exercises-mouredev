5d7aa2c26373aaf2c41ded70522b1bb1a8360572
MSS02204 <u202224602@upc.edu.pe>
2026-01-13 15:04:33 -0500

    feat: add new files

---
+++ Basic/00-helloworld.js
@@ -0,0 +1,29 @@
+//  CTRL + EL BOTONO DE CORCHETE Y LLAVES CON DIRECCION A LA DERECHA
+// Esto es un comentario simple
+
+/*
+Esto es 
+un comentario
+en varias lineas
+*/
+
+console.log("¡Hola, JavaScript!")
+console.log('¡Hola, JavaScript!')
+console.log(`¡Hola, JavaScript!`)
+
+// console.log(`¡Hola, JavaScript!`)
+
+/*
+console.log("¡Hola, JavaScript!")
+console.log('¡Hola, JavaScript!')
+console.log(`¡Hola, JavaScript!`)
+*/
+
+console.log("5")
+console.log(5)
+console.log(5 + 2)
+console.log(5 - 2)
+console.log(5 * 2)
+console.log(5 / 2)
+console.log(5 % 2)
+console.log(5 ** 2)
