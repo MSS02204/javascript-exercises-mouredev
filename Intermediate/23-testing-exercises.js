2c778de6cf49712984bbc90e600da34c8d82a8e3
MSS02204 <u202224602@upc.edu.pe>
2026-02-02 14:46:47 -0500

    feat: Add 23 and 24 files

--- Intermediate/23-testing-exercises.js
+++ Intermediate/23-testing-exercises.js
@@ -1,5 +1,19 @@
 // 1. Crea una función isEven(number) que devuelva true si el número es par y false si es impar
-
 // 2. Escribe una prueba en Jest para verificar que la función funciona correctamente
-
 // 3. Verifique que la prueba se ejecuta satisfactoriamente
+function isEven(number) {
+    let isEven = false
+    if(number % 2 === 0) {
+        isEven = true
+    }
+    return isEven
+}
+
+module.exports = isEven; // Exporta la función isEven para que pueda ser utilizada en otros archivos de prueba.
+
+console.log(isEven(4)) // true
+console.log(isEven(7)) // false
+
+console.log("----------------------")
+
+
---
+++ Intermediate/24-testing-exercises.test.js
@@ -0,0 +1,9 @@
+const isEven = require('./23-testing-exercises');
+
+test("Si el número es par, debe devolver true", () => {
+    expect(isEven(4)).toBe(true)
+})
+
+test("Si el número es impar, debe devolver false", () => {
+    expect(isEven(9)).toBe(false)
+})
