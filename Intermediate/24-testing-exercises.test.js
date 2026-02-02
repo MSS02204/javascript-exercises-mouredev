d5924ab17f06fd6e420ca664e925ebb5d9f7cdcb
MSS02204 <u202224602@upc.edu.pe>
2026-02-02 15:00:56 -0500

    feat: Add 24 file

--- Intermediate/24-testing-exercises.test.js
+++ Intermediate/24-testing-exercises.test.js
@@ -7,3 +7,7 @@ test("Si el número es par, debe devolver true", () => {
 test("Si el número es impar, debe devolver false", () => {
     expect(isEven(9)).toBe(false)
 })
+
+test("Si el número es par, debe devolver true", () => {
+    expect(isEven(12)).toBe(true)
+})
